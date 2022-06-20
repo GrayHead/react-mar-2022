1e94e89bcd52148c38ea12e3b798dffe897a3e53
GrayHead <ss.zhuravlov@gmail.com>
2022-06-20 21:00:24 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import './App.css';
-import Character from "./components/character/Character";
+export  function App() {
+	return (
+		<div>
+		App
 
-function App() {
-    return (<div>
-        <Character
-            name={'BART'}
-            desc={'iergusygj s kfhs kdjfh skdjfh'}
-            picture={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
-        />
-
-        <Character
-            name='Homer'
-            desc='jahyqrfsgdfjgh jsh gfs fsj fgjh'
-            picture='https://static.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png'
-
-        />
-
-
-    </div>);
+		</div>
+	);
 }
-
-export default App;
