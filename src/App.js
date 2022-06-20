c5cadca49714720b7bc8ac87a8e14d59c01f76d2
GrayHead <ss.zhuravlov@gmail.com>
2022-06-20 20:58:46 +0300

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Character from "./components/character/Character";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (<div>
+        <Character
+            name={'BART'}
+            desc={'iergusygj s kfhs kdjfh skdjfh'}
+            picture={'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}
+        />
+
+        <Character
+            name='Homer'
+            desc='jahyqrfsgdfjgh jsh gfs fsj fgjh'
+            picture='https://static.wikia.nocookie.net/simpsons/images/0/02/Homer_Simpson_2006.png'
+
+        />
+
+
+    </div>);
 }
 
 export default App;
