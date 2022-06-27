fc24952fd5efcf03b2a7fb2dc22f3c488c003bbd
GrayHead <ss.zhuravlov@gmail.com>
2022-06-27 21:00:59 +0300

    lesson 3 axios & services

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-export  function App() {
-	return (
-		<div>
-		App
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
+import {useState} from "react";
+import {getPostComments} from "./services";
 
-		</div>
-	);
+export default function App() {
+
+    let [comments, setComments] = useState([]);
+
+    const getPostId = (id) => {
+        getPostComments(id).then(({data}) => setComments([...data]));
+
+    }
+    return (<div>
+
+        <h1>comments of chosen Post</h1>
+        {comments.map(value => <div>{value.body}</div>)
+
+        }
+        <hr/>
+        <br/>
+        <hr/>
+        {/*<Users/>*/}
+        <Posts getPostId={getPostId}/>
+
+
+    </div>);
 }
