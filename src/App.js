14d711a418bd342243483e835dd3d0e43d843aef
GrayHead <ss.zhuravlov@gmail.com>
2022-06-28 20:40:24 +0300

    lesson 3 axios & services

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-export  function App() {
-	return (
-		<div>
-		App
+import Users from "./components/users/Users";
+import {getUserPosts} from "./services/user.api.service";
+import {useState} from "react";
+import './App.css';
+import Posts from "./components/posts/Posts";
 
-		</div>
-	);
+export default function App() {
+
+    let [posts, setPosts] = useState([]);
+    const elevate = (id) => {
+        getUserPosts(id).then(({data}) => {
+            setPosts([...data]);
+        })
+
+    }
+
+    return (<div className={'df'}>
+
+        <div className={'left'}><Users elevate={elevate}/></div>
+        <div className={'right'}>
+            <Posts posts={posts}/>
+
+        </div>
+
+    </div>);
 }
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,6 @@
+export default function Posts({posts}) {
+    return (<div>
+        {posts.map(value => <div key={value.id}>{value.body}</div>)}
+
+    </div>);
+}
