c6f20ff164375fe93f6bd07e5fe73259c7dbd116
GrayHead <ss.zhuravlov@gmail.com>
2022-06-29 21:00:30 +0300

    lesson 4 forms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-export default  function App() {
-	return (
-		<div>
-		App
+import {useForm} from "react-hook-form";
 
-		</div>
-	);
+export default function App() {
+
+    let usersIds = [{id: 1, name: 'asd'},
+        {id: 2, name: '243ssfdsf'},
+        {id: 3, name: '243ssfdsfasdasd'},
+        {id: 4, name: '243ssfssdfdsf'},
+        {id: 5, name: '243ssfdsf'},
+        {id: 6, name: '243ssfdsf'},
+        {id: 7, name: '243ssfdsf'},
+        {id: 8, name: '243ssfdsf'},
+        {id: 9, name: '243ssfdsf'},
+        {id: 10, name: '243ssfdsf'},];
+
+    let {register, handleSubmit, formState: {errors}} = useForm({
+        defaultValues: {
+            title: 'title', body: 'body'
+        }
+    });
+
+
+    let submit = (obj) => {
+        console.log(obj);
+
+        fetch("http://jsonplaceholder.typicode.com/posts", {
+            method: 'POST',
+            body: JSON.stringify(obj),
+            headers: {'Content-type': 'application/json; charset=UTF-8',},
+        })
+            .then(response => response.json())
+            .then(result => console.log(result));
+    };
+
+    return (<div>
+
+        <form onSubmit={handleSubmit(submit)}>
+            <input
+                type="text"
+                {...register("title", {required: true})}/>
+
+            {errors.title && <span> field is required </span>}
+
+            <input
+                type="text"
+                {...register('body')}/>
+
+            <select {...register("userId")}>
+                {usersIds.map(value => <option key={value.id} value={value.id}>{value.name}</option>)}
+
+
+            </select>
+
+
+            <button>save</button>
+        </form>
+
+
+    </div>);
 }
